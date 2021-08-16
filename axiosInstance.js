905ff30b00ff2c0b559b270c8e7476eddeaf4e76
Michael Sanderson <icikle@hotmail.com>
2021-08-16 17:57:52 +0100

    Delete axiosInstance.js

--- axiosInstance.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios')
-const C = require('./constants')
-
-module.exports = axios.create({
-    baseURL: C.baseURL,
-    timeout: C.requestTimeout,
-    headers: { "Authorization": "Bearer TokenGoesHere" }
-  })
